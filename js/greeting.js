b3b64b2019a77adfceb88439e1ba4326cedd2ace
Maxim R. v. Onciul <maxim.onciul@fau.de>
2020-09-19 15:18:53 +0200

    german greet and weather

--- js/greeting.js
+++ js/greeting.js
@@ -2,11 +2,11 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'John Doe';
-var lateTxt = 'Go to Sleep! ';
-var morningTxt = 'Good morning! ';
-var afterTxt = 'Good afternoon ';
-var evenTxt = 'Good evening ';
+var name = 'Maxim';
+var lateTxt = 'Gute Nacht! ';
+var morningTxt = 'Guten Morgen! ';
+var afterTxt = 'Guten Nachmittag ';
+var evenTxt = 'Guten Abend ';
 
 if (Hr >= 0 && Hr < 6) {
     document.getElementById('greetings').innerText = lateTxt + name;
