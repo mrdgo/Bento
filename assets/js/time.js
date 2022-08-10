566cee2fb612becf0fe0c4e20c9b618fcd388b4d
Maxim R. v. Onciul <maxim.ritter.von.onciul@gmail.com>
2022-08-10 15:39:39 +0200

    timezone

--- assets/js/time.js
+++ assets/js/time.js
@@ -3,11 +3,28 @@
 //  ┴ ┴┴ ┴└─┘
 // Set time and Date
 
+function changeTimezone(date, ianatz) {
+
+  // suppose the date is 12:00 UTC
+  var invdate = new Date(date.toLocaleString('en-US', {
+    timeZone: ianatz
+  }));
+
+  // then invdate will be 07:00 in Toronto
+  // and the diff is 5 hours
+  var diff = date.getTime() - invdate.getTime();
+
+  // so 12:00 in Toronto is 17:00 UTC
+  return new Date(date.getTime() - diff); // needs to substract
+
+}
+
 window.onload = displayClock();
 function displayClock() {
 	const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 	var d = new Date();
+	d = changeTimezone(d, 'Germany/Berlin');
 	var mm = monthNames[d.getMonth()];
 	var dd = d.getDate();
 	var min = (mins = ('0' + d.getMinutes()).slice(-2));
--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'M4X1M',
+	name: 'maxim',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
